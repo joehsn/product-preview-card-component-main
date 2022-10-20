ce5256f8d36a6f14339d4ecfd085763e05c8a46f
Youssef Hassan <youssef.hassan.mahmoud@gmail.com>
2022-10-20 11:49:36 -0700

    done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import desktop from "./images/image-product-desktop.jpg";
+import mobile from "./images/image-product-mobile.jpg";
+import cart from "./images/icon-cart.svg";
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+    <div className="app">
+      <div className="card">
+        <div className="w-full md:w-1/2">
+          <picture>
+            <source media="(min-width:768px)" srcSet={desktop} />
+            <img
+              src={mobile}
+              alt="perfume-desktop"
+              className="w-full md:w-auto md:h-full"
+            />
+          </picture>
+        </div>
+        <div className="content">
+          <div className="perfume">perfume</div>
+          <h2 className="title">Gabrielle Essence Eau De Parfum</h2>
+          <div className="brief">
+            A floral, solar and voluptuous interpretation composed by Olivier
+            Polge, Perfumer-Creator for the House of CHANEL.
+          </div>
+          <div className="price">
+            <span className="font-fraunces pr-4">$149.99</span>
+            <span className="line-through text-dark-grayish-blue text-sm">
+              $169.99
+            </span>
+          </div>
+          <button className="btn">
+            <img src={cart} alt="cart icon" className="mr-3 font-montserrat" />{" "}
+            Add to Cart
+          </button>
+        </div>
+      </div>
+      <div className="attribution">
+        Coded by{" "}
         <a
-          className="App-link"
-          href="https://reactjs.org"
+          href="https://linktr.ee/joe_hsn"
+          rel="noreferrer"
           target="_blank"
-          rel="noopener noreferrer"
+          className="underline text-blue-700"
         >
-          Learn React
+          Joe Hsn
         </a>
-      </header>
+        .
+      </div>
     </div>
   );
 }
-
-export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  darkMode: "class",
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "dark-cyan": "hsl(158, 36%, 37%)",
+        cream: "hsl(30, 38%, 92%)",
+        // secondary
+        "very-dark-blue": "hsl(212, 21%, 14%)",
+        "dark-grayish-blue": "hsl(228, 12%, 48%)",
+      },
+      fontFamily: {
+        fraunces: ["Fraunces", "serif"],
+        montserrat: ["Montserrat", "sans-serif"],
+      },
+      fontSize: {
+        paragraph: "14px",
+      },
+    },
   },
   plugins: [],
-}
+};
